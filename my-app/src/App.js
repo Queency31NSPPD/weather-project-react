97d8ebdb6cfda28a807fe3132674b5e74f1a4800
QueenB-31 <125220221+Queency31NSPPD@users.noreply.github.com>
2023-05-21 18:32:36 +0100

    Removed unnecessary files and contents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Weather App
+    <div className="App"> 
+        <p>
+         Weather App
+        </p>
+        Hosted on {""}
+        <a
+          className="App-link"
+          href="https://github.com/Queency31NSPPD/weather-project-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Netlify
+        </a>
     </div>
   );
 }
