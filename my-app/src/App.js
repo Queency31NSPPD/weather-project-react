6aa16d8626ead65ea4a7f422f6239d4b8c73af2c
QueenB-31 <125220221+Queency31NSPPD@users.noreply.github.com>
2023-05-21 19:10:45 +0100

    Updated App.js content

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App"> 
-        <p>
-         Weather App
-        </p>
-        Hosted on {""}
+     <h1> Weather App</h1>
+     <p>
+     This project was Coded by {""}
+     <a
+          className="App-link"
+          href="https://www.linkedin.com/in/bridget-nnenna-chukwu-82a30b149"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Bridget Nnenna
+        </a>
+       {""} and open-sourced on {""}
         <a
           className="App-link"
           href="https://github.com/Queency31NSPPD/weather-project-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-        Netlify
+        Github
         </a>
+          </p>
     </div>
   );
 }
